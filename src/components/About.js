a91b5e88e8329daed9d54090ce21d3cc316dd757
adjudicat0r <mhmdfathan007@gmail.com>
2023-12-13 01:09:43 +0700

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -8,6 +8,9 @@ const About = () => {
       <h2 className="about-title">Tentang Aplikasi Ini</h2>
       <div className="about-info">
         {/* Add information about the application here */}
+        <p>Nama: Muhammad Fathan Mubiina</p>
+        <p>NIM: 21120121140164</p>
+        <p>Kelas: Kriptografi A</p>
         <p>Website Playfair Cypher ini dibuat untuk memenuhi Tugas Remidi UTS Kriptografi</p>
         <p>Mungkin masih terdapat beberapa kekurangan dalam situs web ini, tetapi saya berharap bahwa isinya bermanfaat :)</p>
         {/* Add more information as needed */}
